3058528117f4e7f4f852d63866a9ec21ac109882
PORELARTE <kamranbekirovyz@gmail.com>
2022-10-02 15:39:36 +0400

    Add some basic comments for APIs

--- lib/src/telpo_controller.dart
+++ lib/src/telpo_controller.dart
@@ -10,18 +10,26 @@ class TelpoController {
     _platform = MethodChannel(_channelName);
   }
 
+  // TODO: aljan what are the statuses?
   Future<String?> checkStatus() {
     return _platform.invokeMethod('checkStatus');
   }
 
+  /// Connect with underlying Telpo device if any.
+  ///
+  /// Returns a nullable [bool] whether connected successfully or not.
   Future<bool?> connect() {
     return _platform.invokeMethod('connect');
   }
 
+  /// Disconnect from Telpo device.
+  ///
+  /// Returns a nullable [bool] whether disconnected successfully or not.
   Future<bool?> disconnect() {
     return _platform.invokeMethod('disconnect');
   }
 
+  /// Returns a nullable [bool] whether or not connected with Telpo device.
   Future<bool?> isConnected() {
     return _platform.invokeMethod('isConnected');
   }
