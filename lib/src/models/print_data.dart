e31eb576c5ebabf7a1de36c757692b22e787084f
PORELARTE <kamranbekirovyz@gmail.com>
2022-10-19 12:55:20 +0400

    Make all fields of PrintData private

--- lib/src/models/print_data.dart
+++ lib/src/models/print_data.dart
@@ -1,26 +1,31 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:telpo_flutter_sdk/telpo_flutter_sdk.dart';
+
+/// [Enum] representing printable element type.
+enum _PrintDataType { byte, text, space }
 
 /// Plain Old Dart Object representing printing data.
 ///
 /// Types: [PrintData.text], [PrintData.byte], [PrintData.space]
 class PrintData {
-  final dynamic data;
-  final PrintType type;
-  final double? width;
-  final double? height;
-  final PrintAlignment? alignment;
-  final PrintedFontSize? fontSize;
+  final dynamic _data;
+  final double? _width;
+  final double? _height;
+  final PrintAlignment? _alignment;
+  final PrintedFontSize? _fontSize;
+
+  final _PrintDataType _type;
 
   /// [PrintData] can be initialized through factory constructors: text, byte, space.
-  const PrintData._({
-    required this.type,
-    required this.data,
-    this.width,
-    this.height,
-    this.alignment,
-    this.fontSize,
-  });
+  const PrintData._(
+    this._type,
+    this._data, [
+    this._width,
+    this._height,
+    this._alignment,
+    this._fontSize,
+  ]);
 
   /// PrintData from text. Optional alignment (PrintAlignment) and fontSize
   /// (PrintedFontSize) can be assigned for styling.
@@ -30,10 +35,12 @@ class PrintData {
     PrintedFontSize? fontSize,
   }) {
     return PrintData._(
-      type: PrintType.text,
-      data: text,
-      alignment: alignment,
-      fontSize: fontSize,
+      _PrintDataType.text,
+      text,
+      null,
+      null,
+      alignment,
+      fontSize,
     );
   }
 
@@ -42,10 +49,7 @@ class PrintData {
   ///
   /// line property represents count of lines to be inserted.
   factory PrintData.space({required int line}) {
-    return PrintData._(
-      type: PrintType.walkPaper,
-      data: line,
-    );
+    return PrintData._(_PrintDataType.space, line);
   }
 
   /// PrintData from list of bytes where bytesList property can be comprised of
@@ -58,24 +62,24 @@ class PrintData {
     PrintedFontSize? fontSize,
   }) {
     return PrintData._(
-      type: PrintType.byte,
-      data: bytesList,
-      width: width,
-      height: height,
-      alignment: alignment,
-      fontSize: fontSize,
+      _PrintDataType.byte,
+      bytesList,
+      width,
+      height,
+      alignment,
+      fontSize,
     );
   }
 
   /// Data is being transferred to the plugin as JSON.
   Map<String, dynamic> toJson() {
     return {
-      "data": data,
-      "width": width,
-      "height": height,
-      "alignment": alignment?.name,
-      "type": type.name,
-      "fontSize": fontSize?.name,
+      "data": _data,
+      "width": _width,
+      "height": _height,
+      "alignment": _alignment?.name,
+      "type": _type.name,
+      "fontSize": _fontSize?.name,
     };
   }
 
@@ -83,11 +87,11 @@ class PrintData {
   @override
   String toString() {
     return '''PrintModel(
-      data: $data,
-      width: $width,
-      height: $height,
-      alignment: $alignment
-      type: $type,
-      fontSize: $fontSize)''';
+      data: $_data,
+      width: $_width,
+      height: $_height,
+      alignment: $_alignment
+      type: $_type,
+      fontSize: $_fontSize)''';
   }
 }
