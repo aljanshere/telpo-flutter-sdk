92fbf213327ed97f75b492dd4844764afffdd2bd
PORELARTE <kamranbekirovyz@gmail.com>
2022-10-19 13:02:00 +0400

    Documentation for enums (PrintResult, TelpoStatus, PrintedFontSize)

--- lib/src/utilities/enums.dart
+++ lib/src/utilities/enums.dart
@@ -1,15 +1,16 @@
-enum PrintType {
-  byte,
-  text,
-  walkPaper,
-}
-
+/// [Enum] representing alignment of [PrintData.text] element.
 enum PrintAlignment {
+  /// Left
   left,
+
+  /// Center
   center,
+
+  /// Right
   right,
 }
 
+/// [Enum] representing sizes of [PrintData.text] element.
 enum PrintedFontSize {
   size18,
   size24,
@@ -19,22 +20,41 @@ enum PrintedFontSize {
   size64,
 }
 
-// TODO: describe each status
+/// [Enum] representing status of underlying Telpo device.
 enum TelpoStatus {
+  /// OK, meaning able to print.
   ok,
+
+  /// No paper left on device.
   noPaper,
+
+  /// Device is overheated.
   overHeat,
+
+  /// Device cache is full, (@mrjnlcn what does that mean?)
   cacheIsFull,
+
+  /// Any other unknown and undocumented status.
   unknown,
 }
 
-// TODO: describe each status
-// TODO: add exception reason to other
+/// [Enum] representing result of printing action.
 enum PrintResult {
+  /// Printed successfully.
   success,
+
+  /// Printed aborted since there is no paper left.
   noPaper,
+
+  /// Printed aborted due to insufficient battery.
   lowBattery,
+
+  /// Printed aborted since the device is overheated.
   overHeat,
+
+  /// Printed aborted since device is not ready for the action (weird, right?).
   dataCanNotBeTransmitted,
+
+  /// Printed aborted because of undocumented and unexpected reason.
   other,
 }
