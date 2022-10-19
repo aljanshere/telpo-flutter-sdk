ef156f45d78dd8ce4941cce9feee4ac18842cd2c
PORELARTE <kamranbekirovyz@gmail.com>
2022-10-19 13:02:23 +0400

    Update library imports

--- lib/telpo_flutter_sdk.dart
+++ lib/telpo_flutter_sdk.dart
@@ -2,5 +2,5 @@ library telpo_flutter_sdk;
 
 export 'src/telpo_flutter_channel.dart';
 export 'src/models/print_data.dart';
+export 'src/models/telpo_print_sheet.dart';
 export 'src/utilities/enums.dart';
-export 'src/utilities/extensions.dart';
